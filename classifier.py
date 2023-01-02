b7cdf753307b316656f7c56091c47bac623bb8e5
Gökhan Koçmarlı <gokhankocmarli@gmail.com>
2023-01-02 16:48:14 +0300

    refactor: Clean usage

--- classifier.py
+++ classifier.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         "-w",
         "--weights",
         nargs="+",
-        required=True,
+        required=False,
         help="Provide the weights file location.",
     )
     parser.add_argument(
@@ -24,16 +24,28 @@ if __name__ == "__main__":
         "-m",
         "--mapper",
         nargs="+",
-        required=True,
+        required=False,
         help="Provide the mapper (words to vector) file location.",
     )
     args = parser.parse_args()
 
     # Load the model weights.
-    weights = BLRFileManager.load_weights(args.weights[0])
+    if args.weights is None:
+        weights_file: str = "best_weights.tsv"
+        print("No weights file provided. Using the default weights file:", weights_file)
+    else:
+        weights_file = args.weights[0]
+
+    weights = BLRFileManager.load_weights(weights_file)
 
     # Convert the review to a vector.
-    mapper = W2NFileManager.load_feature_dictionary(args.mapper[0])
+    if args.mapper is None:
+        mapper_file: str = "dataset/word2vec.txt"
+        print("No mapper file provided. Using the default mapper file:", mapper_file)
+    else:
+        mapper_file = args.mapper[0]
+
+    mapper = W2NFileManager.load_feature_dictionary(mapper_file)
     review: str = " ".join(args.review)
     review_numeric = Words2Numbers(mapper).convert(review)
 
